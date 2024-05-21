0f5c9b7eeb93e54a1a7599b6dbe4babfbf2fc912
Niels Demeyer <niels.demeyer99@gmail.com>
2024-05-21 11:59:36 +0200

    chore: Refactor MlflowClass_file.py for improved data preprocessing and pipeline creation

--- MlflowClass_file.py
+++ MlflowClass_file.py
@@ -4,6 +4,10 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pandas as pd
+from sklearn.pipeline import make_pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
 
 
 class RandomForestMLflow:
@@ -11,6 +15,7 @@ class RandomForestMLflow:
         self.df = pd.read_csv(filepath)
         self.clean_df()
         self.split_data()
+        self.create_pipeline()
 
     def clean_df(self):
         self.df = self.df[self.df.columns[:-2]]
@@ -21,4 +26,30 @@ class RandomForestMLflow:
         y = self.df["Attrition_Flag"]
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
             X, y, test_size=0.2, random_state=42
+        )
+
+    def create_pipeline(self):
+        # Identifying categoricals and numericals
+        categorical_cols = self.X_train.select_dtypes(
+            include=["object", "category"]
+        ).columns
+        numerical_cols = self.X_train.select_dtypes(
+            exclude=["object", "category"]
+        ).columns
+
+        # Numerical preprocessing
+        numerical_pipeline = make_pipeline(
+            SimpleImputer(strategy="mean"), StandardScaler()
+        )
+
+        # Categorical preprocessing
+        categorical_pipeline = make_pipeline(OneHotEncoder(handle_unknown="ignore"))
+
+        # ColumnTransformer
+        self.preprocessor = ColumnTransformer(
+            transformers=[
+                ("cat", categorical_pipeline, categorical_cols),
+                ("num", numerical_pipeline, numerical_cols),
+            ],
+            remainder="passthrough",
         )
