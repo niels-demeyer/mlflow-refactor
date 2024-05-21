347f2af7e12530dfd75523dc4f724c114b89414b
Niels Demeyer <niels.demeyer99@gmail.com>
2024-05-21 11:57:13 +0200

    chore: Refactor MlflowClass_file.py for improved data cleaning and splitting

--- MlflowClass_file.py
+++ MlflowClass_file.py
@@ -3,29 +3,22 @@ import mlflow.sklearn
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import pandas as pd
 
 
 class RandomForestMLflow:
-    def __init__(self, data, target):
-        self.data = data
-        self.target = target
+    def __init__(self, filepath):
+        self.df = pd.read_csv(filepath)
+        self.clean_df()
+        self.split_data()
+
+    def clean_df(self):
+        self.df = self.df[self.df.columns[:-2]]
+        self.df = self.df.drop(["CLIENTNUM"], axis=1)
+
+    def split_data(self):
+        X = self.df.drop("Attrition_Flag", axis=1)
+        y = self.df["Attrition_Flag"]
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
-            data, target, test_size=0.2, random_state=42
+            X, y, test_size=0.2, random_state=42
         )
-
-    def train_model(self, n_estimators=100, max_depth=None):
-        self.model = RandomForestClassifier(
-            n_estimators=n_estimators, max_depth=max_depth
-        )
-        self.model.fit(self.X_train, self.y_train)
-
-    def evaluate_model(self):
-        self.predictions = self.model.predict(self.X_test)
-        self.accuracy = accuracy_score(self.y_test, self.predictions)
-
-    def log_mlflow(self):
-        with mlflow.start_run():
-            mlflow.log_param("n_estimators", self.model.n_estimators)
-            mlflow.log_param("max_depth", self.model.max_depth)
-            mlflow.log_metric("accuracy", self.accuracy)
-            mlflow.sklearn.log_model(self.model, "model")
