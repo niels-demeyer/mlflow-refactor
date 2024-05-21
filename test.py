750dfdf7a6269febe85cd42aaa5e6a4a9cdf1b6a
Niels Demeyer <niels.demeyer99@gmail.com>
2024-05-21 12:18:39 +0200

    chore: Update filepath in test.py for BankChurners dataset

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
 from MlflowClass_file import RandomForestMLflow
 
-ml = RandomForestMLflow()
-
-
 # define the filepath
 filepath = "./customer_churn_analysis/data/BankChurners.csv"
 rf = RandomForestMLflow(filepath=filepath)
